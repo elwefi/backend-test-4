7c50de779cbd405fdaf2370b16ed2af74fa8685a
ahmed elwafi <ahmed.elwefi@gmail.com>
2018-03-26 00:58:08 +0200

    remove comment

--- app/controllers/api/twilio_controller.rb
+++ app/controllers/api/twilio_controller.rb
@@ -4,21 +4,15 @@ class Api::TwilioController < ApplicationController
   def ivr_welcome
     service = Twilio::IvrCallService.new(@call, call_params, params['Digits'])
     service.perform
-
-    p service.response.to_s
-    p service.success
     return render xml: service.response.to_s, status: :ok if service.success
   end
 
   # Update the status whene the call is completed.
   def call_status_change
-    p "call status change"
-    success = @call.update_attributes(
+    @call.update_attributes(
       status: params['CallStatus'],
       duration: params['CallDuration']
     )
-    return head :ok if success
-    render json: false, status: :unprocessable_entity
   end
 
   private
